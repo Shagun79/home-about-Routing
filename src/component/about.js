e45d0bce98e2e89ffb6c52e720de1668a61bccf8
Shagun <jaiswalshagun79@gmail.com>
2023-09-24 13:46:51 +0530

    home about routing

---
+++ src/component/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./navbarr";
+import "./routing.css";
+const About = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to Vezber </h1>
+      <h1> About Page</h1>;
+    </>
+  );
+};
+
+export default About;
---
+++ src/component/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./navbarr";
+import "./routing.css";
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <h1> Vezber</h1>
+      <h1>Home Page</h1>;
+    </>
+  );
+};
+
+export default Home;
